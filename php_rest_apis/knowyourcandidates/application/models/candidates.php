1dde32f30a85b166188af720c290f5416cd94821
Vasanth Vallabh <vvallabb@gmail.com>
2014-02-20 06:28:28 +0530

    persist loksabha 15 531 mps data
    As part of this commit. All the MP data is persisted for loksabha 15.
    Keys were modified to match the db columns in mps.json. Still
    Insert_batch to be used to make writes faster. But, it is advisable not
    to use insert_Batch if there are more than 10,000 records. Under data
    set loksabha_15_531_mps.json is placed, this is our master json file.

--- php_rest_apis/knowyourcandidates/application/models/candidates.php
+++ php_rest_apis/knowyourcandidates/application/models/candidates.php
@@ -13,48 +13,60 @@ class Candidates extends CI_Model {
 	* This method parses the json from a file and persists to a DB table
 	Example:
 	{
-    "Place of Birth": "Bero, Distt. Purulia (West Bengal)",
-    "No.of Daughters": "2",
-    "Mother's Name": "Smt. Konak Lata Acharia",
-    "Party Name": "Communist Party of India (Marxist)(CPI(M))",
-    "Profession": "Trade Unionist Social Worker",
-    "Date of Marriage": "25 Feb 1975",
-    "Constituency": "Bankura (West Bengal )",
-    "Date of Birth": "11 Jul 1942",
-    "Educational Qualifications": "M.A.,B.T. Educated at Ranchi University, Ranchi, Bihar and Kolkata University, Kolkata, West Bengal",
-    "No. of Sons": "1",
-    "Other Information": "(i) West Bengal Railway Contracter Labour Union; (ii) Purulia District Committee (CITU); (iii) LIC Agent organisation of India; (iv) DVC Contractor Workers Union; (v) Santhaldihi Thermal Power Plant, Thikader Shramik Union; (vi) Damodar Cement and Slag Workers Union; (i) All India Centre, Indian Trade Union; (ii) DVC Shramik Union and (iii) Colliery Mazdoor Sabha of India; (i) Central Committee CPI (M); (ii) General Council, CITU; (iii) Working Committee, CITU, West Bengal",
-    "Present Address": "\n21, Ashoka Road,\nNew Delhi - 110 001\nTels. (011) 23342235, 23362235, 23034705 9868180022(M), 09434014347 (M)\n\n\n\n",
-    "Email Address :": "bacharya@sansad.nic.in",
-    "Name": "Acharia,Shri Basudeb",
-    "Marital Status": "Married",
-    "Countries Visited": "Iraq (Baghdad), Former U.S.S.R.; Member, Indian Parliamentary Delegations to Mongolia, (1985); and U.K., (1990); participated in International Conference on `Peace and Solidarity` at Pyongyang, North Korea; visited Oman, Poland, Czech Republic, Italy and Slovak Republic as member of Delegation led by the President of India, (1996); attended (i) UN General Assembly Session, (1999); (ii) World Solidarity Conference, Havana, (2000); (iii) Solidarity and fraternal relation between CITU and Zenroren, National Confederation of Trade Unions, Tokyo,Japan,(September,2009) and the World Federation of Trade Unions, Brussels, (October 2009)",
-    "Image": "http://164.100.47.132/mpimage/photo/4.jpg",
-    "Positions Held": "\n1980\nElected to 7th Lok Sabha\n\n1981\nMember, Secretariat, Communist Party of India (Marxist) [C.P.I.(M)], Distt. Committee, Purulia\nMember, State Committee, C.P.I.(M), West Bengal\n\n1984\nRe-elected to 8th Lok Sabha (2nd term)\n\n1985 onwards\nMember, State Committee, C.P.I.(M), West Bengal\n\n1985-89\nMember, Railway Convention Committee\n\n1986-89\nMember, Joint Select Committee on Railways Bill\n\n1989\nRe-elected to 9th Lok Sabha (3rd term)\n\n1990-91\nChairman, Committee on Public Undertakings\nMember, House Committee\nMember, General Purposes Committee\n\n1990-96\nMember, Consultative Committee, Ministry of Railways\n\n1991\nRe-elected to 10th Lok Sabha (4th term)\n\n1993-95\nMember, Committee on Public Undertakings\n\n1993-96\nChairman, Committee on Government Assurances\nMember, Committee on Railways\n\n1996\nRe-elected to 11th Lok Sabha (5th term)\n\n1996-97\nChairman, Committee on Railways\nMember, Panel of Chairman\nMember, Consultative Committee, Ministry of Industry\n\n1998\nRe-elected to 12th Lok Sabha (6th term)\nMember, Panel of Chairman, Lok Sabha\n\n1998-99\nConvenor, Sub-Committee on Power, Committee on Energy\nMember, Committee on Energy and its Sub-Committees on Coal and Action Taken Reports\nMember, Committee of Privileges\nMember, General Purposes Committee\nMember, Committee to review the rate of dividend payable by the Railway Undertakings to General Revenues\nMember, Consultative Committee, Ministry of Industry\nSpecial Invitee, Consultative Committee, Ministry of Railways\n\n1999\nRe-elected to 13th Lok Sabha (7th term)\n\n1999-2004\nChairman, Committee on Petitions\nMember, Committee on Energy\nMember, General Purposes Committee\n\n2000-2004\nMember, Consultative Committee Ministry of Railways Special Member, Consultative Committee, Ministry of Industry\n\n2004\nRe-elected to 14th Lok Sabha( 8th term) Leader, CPI (M) Parliamentary Party, Lok Sabha Chairman, Committee on Railways Member, Rules Committee Member, General Purposes Committee\nMember, Committee on Security in Parliament Complex\nMember, Committee on Installation of Portraits/Statues of National Leaders and Parliamentarian in Parliament House\nMember, Consultative Committee, Ministry of Heavy Industry & Public Enterprises\nSpecial Member, Consultative Committee, Ministry of Power\n\n5 Aug. 2007\nChairman, Committee on Railways\n\n2008\nMember, Central Committee CPI (M)\n\n2009\nRe-elected to 15th Lok Sabha (9th term)\nLeader, CPI (M) Parliamentary Party, Lok Sabha\n\nJun. 2009\nMember, Panel of Chairmen, Lok Sabha\n\n29 Jun. 2009\nMember, Business Advisory Committee\n\n31 Aug. 2009\nChairman, Committee on Agriculture\n\n19 Oct. 2009\nMember, General Purposes Committee\nMember, Consultative Committee, Ministry of Heavy Industries & Public Enterprises\nSpecial Member, Consultative Committee, Ministry of Power\n",
-    "Permanent Address": "\nVillage Kantaranguni, P.O. Adra,\nDistt. Purulia - 723 101, West Bengal\nTels.(03251) 244262,244900 Fax. (03252) 222360\n\n\n\n",
-    "Father's Name": "Late Shri Kanailal Acharia",
-    "Spouse's Name": "Smt. Rajlaxmi Acharia"
+		"placeofbirth": "Chennai (Tamil Nadu)",
+		"socialandculturalactivities": "Working for the upliftment of the backward, dalits and minorities",
+		"sportsandclubs": "Football and kabbadi",
+		"mothername": "Smt. S.J.M.H. Bivi",
+		"partyname": "Indian National Congress(INC)",
+		"favoritepasttimeandrecreation": "Public speaking",
+		"specialinterests": "Promoting communal harmony in remote dalit villages",
+		"otherinformation": ", Rubber Board, Ministry of Commerce and Industry",
+		"emailid": "jm.aaronrasheed@sansad.nic.in",
+		"presentaddress": "\n3, Mahadev Road,\nNew Delhi - 110 001\nTel. (011) 23355101, Fax. (011) 23355102 9868180133 (M)\n\n\n\n",
+		"candidateid": "4064",
+		"imageurl": "http://164.100.47.132/mpimage/photo/4064.jpg",
+		"permanentaddress": "\n(i) 7, VRC Road, Street - I\nTeynampet, Chennai - 600 002 Tamil Nadu\nTels.(044) 28156283, 09444048611(M)\n(ii) 126-Cumbum Road, P.C. Patti, Theni- 625 531 Tamil Nadu Tel. (04546) 265699\n\n\n",
+		"spousename": "Smt. J.M.H.Hazaran Bi",
+		"noofdaughters": "2",
+		"marriagedate": "29 Nov 1978",
+		"profession": "Businessperson Agriculturist",
+		"constituency": "Theni (Tamil Nadu )",
+		"dateofbirth": "13 May 1950",
+		"noofsons": "2",
+		"education": "Intermediate Educated at Sir Thiyagaraja College, Washermanpet, Chennai, Tamil Nadu",
+		"name": "Aaroon Rasheed,Shri J.M.",
+		"maritalstatus": "Married",
+		"countriesvisited": "Widely travelled",
+		"positionsheld": "\n1996-2001\nMember, Tamil Nadu Legislative Assembly\n\n2004\nElected to 14th Lok Sabha\nMember, Committee on Energy\nMember, Consultative Committee, Ministry of Commerce and Industry\nMember, Consultative Committee, Ministry of Shipping\nMember, Committee on Government Assurances\n\n2 Jan 2006\nMember, Joint Parliamentary Committee on Wakf\n\n5 Aug 2007\nMember, Committee on Energy\n\n2009\nRe-elected to 15th Lok Sabha (2nd term)\n\n31 Aug 2009\nMember, Committee on Health and Family Welfare\nMember, Committee on Official Languages\nMember, Consultative Committee, Ministry of Shipping\n",
+		"fathername": "Shri Jamal Mohideen"
 	}
 	*/
 	public function persist_candidates(){
-	   $MP_JSONS_DIR = 'TODO: MP JSON FILES DIRECTORY';
-	   
-	   if ($handle = opendir($MP_JSONS_DIR)) {
-        while (false !== ($mpjsonfilename = readdir($handle))) {
-			$ext = pathinfo($mpjsonfilename, PATHINFO_EXTENSION);
-            if ($mpjsonfilename != "." && $mpjsonfilename != ".." && $ext == "json") {
-				$file = file_get_contents($MP_JSONS_DIR."/".$mpjsonfilename, FILE_USE_INCLUDE_PATH);
-				$json_arry = json_decode($file,true);
-				/**
-				* TODO: 
-				* For some MPs mother's name is missing. Need to handle that.
-				* Also, need to check for all the fields. Index not found checks to be added.
-				*/				
-				print $json_arry["Place of Birth"] . " " . $json_arry["Mother's Name"] . "\n";
-				//TODO: Save to Database pending. Print statement is temporary and to be removed after persistent logic is added.
-            }
-        }
-        closedir($handle);
+		$MP_JSONS_DIR = 'C:\Users\vasanth\Documents\GitHub\14feb2014\opengovtdata\opengovtdata\jsoupscraper\mpfiles';
+
+		if ($handle = opendir($MP_JSONS_DIR)) {
+			while (false !== ($mpjsonfilename = readdir($handle))) {
+				$ext = pathinfo($mpjsonfilename, PATHINFO_EXTENSION);
+				if ($mpjsonfilename != "." && $mpjsonfilename != ".." && $ext == "json") {
+					$file = file_get_contents($MP_JSONS_DIR."/".$mpjsonfilename, FILE_USE_INCLUDE_PATH);
+					$json_arry = json_decode($file,true);
+					
+					//TODO: To replace this with batch insert 
+					foreach ($json_arry as $json_arr){
+						$this->db->insert('knowyourcandidates.loksabha_15_531', $json_arr); 
+					}
+					
+					//TODO: The following line doesn't work and it gives this error
+					/**
+					* A Database Error Occurred
+					* Error Number: 1064
+					* You have an error in your SQL syntax; check the manual that corresponds to your MySQL 
+					* server version for the right syntax to use near 'Array' at line 1
+					*/
+					//$this->db->insert_batch('knowyourcandidates.loksabha_15_531',$json_arry); 
+				}
+			}
+			closedir($handle);
 		}
 	}
 }
