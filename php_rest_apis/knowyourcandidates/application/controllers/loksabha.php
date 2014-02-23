2ac958a5b6c13bdc6f895ad9e2ded76768d8b307
Vasanth Vallabh <vvallabb@gmail.com>
2014-02-23 20:48:49 +0530

    REST, xml end points
    By placing rest.php in config folder and REST_controller.php ,Format.php
    libraries, our backend is now capable of handling HTTP methods and also
    works as end points.
    
    Ref: https://github.com/vvallabb/codeigniter-restserver

---
+++ php_rest_apis/knowyourcandidates/application/controllers/loksabha.php
@@ -0,0 +1,33 @@
+<?php
+
+require(APPPATH.'libraries/REST_Controller.php');
+ 
+class loksabha extends REST_Controller {
+	function user_get()
+    {
+       $data = array('returned: '. $this->get('id'));
+        $this->response($data);
+		
+    }
+     
+    function user_post()
+    {       
+        $data = array('returned: '. $this->post('id'));
+        $this->response($data);
+    }
+ 
+    function user_put()
+    {       
+        $data = array('returned: '. $this->put('id'));
+        $this->response($data);
+    }
+ 
+    function user_delete()
+    {
+        $data = array('returned: '. $this->delete('id'));
+        $this->response($data);
+    }
+}
+
+
+?>
